63a194d383750c4b74f103872cb354651d1b1e7f
Jennifer Jones <jybjones@gmail.com>
2015-07-18 12:20:11 -0500

    changed the default route

--- www/js/app.js
+++ www/js/app.js
@@ -95,5 +95,5 @@ angular.module('starter', [
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/search');
 });
